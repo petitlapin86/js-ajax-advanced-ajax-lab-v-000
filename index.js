7c5cc222f80362c8cd4998fdba759a5c8f5e2b62
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-22 13:20:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function showRepositories(event, data) {
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText)
+  const repoList = `<ul>${repos.map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
